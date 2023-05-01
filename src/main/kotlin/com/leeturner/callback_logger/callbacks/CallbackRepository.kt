8cbb9bde3c7bd201a6c26160f80172d35bf46d9c
leeturner <lee@leeturner.me>
2023-05-01 09:11:19 +0100

    Moved to a domain based package structure and made sure we return the callbacks in descending order by timestamp

--- src/main/kotlin/com/leeturner/callback_logger/callbacks/CallbackRepository.kt
+++ src/main/kotlin/com/leeturner/callback_logger/callbacks/CallbackRepository.kt
@@ -1,11 +1,10 @@
-package com.leeturner.callback_logger.repository
+package com.leeturner.callback_logger.callbacks
 
-import com.leeturner.callback_logger.domain.Callback
 import io.micronaut.data.jdbc.annotation.JdbcRepository
 import io.micronaut.data.model.query.builder.sql.Dialect
 import io.micronaut.data.repository.CrudRepository
 
 @JdbcRepository(dialect = Dialect.H2)
 interface CallbackRepository : CrudRepository<Callback, Long> {
-    fun listOrderByTimestamp(): List<Callback>
+    fun listOrderByTimestampDesc(): List<Callback>
 }
