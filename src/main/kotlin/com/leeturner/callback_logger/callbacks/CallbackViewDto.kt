1ddeaddede8992a4d5daed6d1c549a1f604c3d2b
leeturner <lee@leeturner.me>
2023-05-01 09:07:18 +0100

    Moved to a domain based package structure and renamed the dto class

---
+++ src/main/kotlin/com/leeturner/callback_logger/callbacks/CallbackViewDto.kt
@@ -0,0 +1,19 @@
+package com.leeturner.callback_logger.callbacks
+
+import io.micronaut.serde.annotation.Serdeable
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.time.format.FormatStyle
+
+@Serdeable
+data class CallbackViewDto(
+    val id: Long,
+    val status: String,
+    val timestamp: LocalDateTime,
+    val httpMethod: String,
+    val httpVersion: String,
+    val payload: String,
+) {
+  val formattedTimestamp: String
+    get() = this.timestamp.format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM))
+}
