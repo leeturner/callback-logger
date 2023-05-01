92674380331f7f8c20b679581f24060708036119
leeturner <lee@leeturner.me>
2023-05-01 08:57:16 +0100

    Add in some simple reusable test data

---
+++ src/test/kotlin/com/leeturner/callback_logger/TestFixtures.kt
@@ -0,0 +1,8 @@
+package com.leeturner.callback_logger
+
+object TestFixtures {
+    
+    const val TEST_JSON_PAYLOAD = """{"hello": "world", "foo": "bar"}"""
+
+    const val TEST_XML_PAYLOAD = """<a><test>XML Payload</test></a>"""
+}
