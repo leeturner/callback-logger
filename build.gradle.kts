30dcbe885b0f147e6db2c8774e88b7dc8b467371
leeturner <lee@leeturner.me>
2023-05-01 09:05:32 +0100

    Upgraded Micronaut, removed some unused dependencies and added junit and rest-assured

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
   id("org.jetbrains.kotlin.kapt") version "1.6.21"
   id("org.jetbrains.kotlin.plugin.allopen") version "1.6.21"
   id("com.github.johnrengelman.shadow") version "7.1.2"
-  id("io.micronaut.application") version "3.7.8"
+  id("io.micronaut.application") version "3.7.9"
 }
 
 version = "0.1"
@@ -18,23 +18,22 @@ dependencies {
   kapt("io.micronaut.data:micronaut-data-processor")
   kapt("io.micronaut:micronaut-http-validation")
   kapt("io.micronaut.serde:micronaut-serde-processor")
-  implementation("io.micronaut:micronaut-http-client")
   implementation("io.micronaut:micronaut-jackson-databind")
   implementation("io.micronaut.data:micronaut-data-jdbc")
-  implementation("io.micronaut.serde:micronaut-serde-jackson")
   implementation("io.micronaut.kotlin:micronaut-kotlin-extension-functions")
   implementation("io.micronaut.kotlin:micronaut-kotlin-runtime")
   implementation("io.micronaut.sql:micronaut-jdbc-hikari")
   implementation("io.micronaut.views:micronaut-views-thymeleaf")
-  implementation("io.micronaut:micronaut-validation")
   implementation("jakarta.annotation:jakarta.annotation-api")
   implementation("org.jetbrains.kotlin:kotlin-reflect:${kotlinVersion}")
   implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:${kotlinVersion}")
   runtimeOnly("ch.qos.logback:logback-classic")
   implementation("com.h2database:h2")
-  runtimeOnly("com.fasterxml.jackson.module:jackson-module-kotlin")
 
+  testImplementation(platform("org.junit:junit-bom:5.9.3"))
+  testImplementation("org.junit.jupiter:junit-jupiter")
   testImplementation("io.strikt:strikt-core:0.34.0")
+  testImplementation("io.micronaut.test:micronaut-test-rest-assured")
 }
 
 application { mainClass.set("com.leeturner.callback_logger.ApplicationKt") }
