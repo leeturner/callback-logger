2dac39c1dd3945daf1166c7d964cb3b28e6203ed
leeturner <lee@leeturner.me>
2023-05-04 09:06:16 +0100

    Comment out the start of the H2 web console

--- src/main/kotlin/com/leeturner/callback_logger/Application.kt
+++ src/main/kotlin/com/leeturner/callback_logger/Application.kt
@@ -1,11 +1,10 @@
 package com.leeturner.callback_logger
 
-import org.h2.tools.Server
 import io.micronaut.runtime.Micronaut.run
 
 fun main(args: Array<String>) {
   // this is just used for development purposes to start the h2 console.
   // can be removed.  The console can be accessed on http://localhost:8082/
-  Server.createWebServer().start()
+  //  Server.createWebServer().start()
   run(*args)
 }
