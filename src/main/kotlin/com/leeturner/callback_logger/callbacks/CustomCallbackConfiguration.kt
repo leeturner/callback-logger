2225b1f2c8d599c9c07be7f9bd03cca03985fc80
leeturner <lee@leeturner.me>
2023-06-10 22:35:57 +0100

    data class for the configuration of the custom callbacks

--- src/main/kotlin/com/leeturner/callback_logger/callbacks/CustomCallbackConfiguration.kt
+++ src/main/kotlin/com/leeturner/callback_logger/callbacks/CustomCallbackConfiguration.kt
@@ -3,7 +3,7 @@ package com.leeturner.callback_logger.callbacks
 import io.micronaut.context.annotation.ConfigurationProperties
 
 @ConfigurationProperties("callback-logger.custom-callbacks")
-data class CustomCallbackConfiguration (
-  val post: List<String>,
-  val put: List<String>,
+data class CustomCallbackConfiguration(
+    val post: List<String> = emptyList(),
+    val put: List<String> = emptyList(),
 )
