66e0947a1a15959382cbbc8935a4e2005441626b
leeturner <lee@leeturner.me>
2025-02-03 16:48:53 +0000

    remove debug and change time

--- src/test/kotlin/com/leeturner/callback_logger/callbacks/CallbackViewDtoTest.kt
+++ src/test/kotlin/com/leeturner/callback_logger/callbacks/CallbackViewDtoTest.kt
@@ -13,7 +13,7 @@ class CallbackViewDtoTest {
           id = 1L,
           status = CallbackStatus.NEW.name,
           uri = "/api/callback",
-          timestamp = LocalDateTime.parse("2023-04-29T23:41:54.058478"),
+          timestamp = LocalDateTime.parse("2023-04-29T10:41:54.058478"),
           httpMethod = "POST",
           httpVersion = "HTTP_1.1",
           payload = TestFixtures.TEST_JSON_PAYLOAD,
@@ -21,7 +21,6 @@ class CallbackViewDtoTest {
 
   @Test
   internal fun `timestamps are formatted correctly for the view`() {
-    println(testCallback.formattedTimestamp)
     expectThat(testCallback.formattedTimestamp).contains("29 Apr 2023")
   }
 }
